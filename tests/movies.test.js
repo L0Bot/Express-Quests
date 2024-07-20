175fe7ca7b98e32ef45e7ae73617e4953e9e0d59
L0Bot <botterilouis@gmail.com>
2024-07-20 17:59:42 +0200

    test good

--- tests/movies.test.js
+++ tests/movies.test.js
@@ -1,6 +1,8 @@
 const request = require("supertest");
-
+const database = require("../database.js");
 const app = require("../src/app");
+
+afterAll(() => database.end());
 
 describe("GET /api/movies", () => {
   it("should return all movies", async () => {
